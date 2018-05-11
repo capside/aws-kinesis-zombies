1162e6d9855167730f95964c0393c3c58bc6e4f4
Javi Moreno (@ciberado) <javi@programar.cloud>
2018-05-11 14:37:21 +0200

    Fixed cron task (deactivated by default with version 2 of boot)

--- ZombieConsumer/src/main/java/com/capside/realtimedemo/consumer/Application.java
+++ ZombieConsumer/src/main/java/com/capside/realtimedemo/consumer/Application.java
@@ -10,10 +10,12 @@ package com.capside.realtimedemo.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Application {
-
+    
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
